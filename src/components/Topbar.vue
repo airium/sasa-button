9786c6b7c5f51147f71c5713d49a1a7e27ffd2ee
Nyaasu <nyaasu@qq.com>
2020-06-28 15:57:24 +0800

    微调按钮图标大小

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -141,7 +141,7 @@ export default {
 
 .iconfont {
   font-family: "iconfont" !important;
-  font-size: 16px;
+  font-size: 20px !important;
   font-style: normal;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
